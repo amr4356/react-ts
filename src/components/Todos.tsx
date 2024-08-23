1e3a5d120b848d17e0603e62646c04142e4117e1
amr4356 <amrooo73321@gmail.com>
2024-08-23 04:02:27 +0300

    571. The Context API & TypeScript

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import Todo from "../models/todo";
+import React, { useContext } from "react";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
+import { TodosContext } from "../store/todos-context";
 
-const Todos: React.FC<{items: Todo[]; onDeleteTodo: (id : string) => void}> = (props) => {
+const Todos: React.FC = () => {
+
+    const todosCtx=useContext(TodosContext);
     return (
         <ul className={classes.todos}>
-           {props.items.map( (item) => 
-                <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onDeleteTodo.bind(null, item.id)} />
+           {todosCtx.items.map( (item) => 
+                <TodoItem key={item.id} text={item.text} onRemoveTodo={() => todosCtx.removeTodo(item.id)} />
            )} 
         </ul>
     );
